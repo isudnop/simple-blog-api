0ac1df5dd7fd79fb59ec8acc8e37265ae5b61c06
Noppadol Mututanon <noppadol.mututanon@gmail.com>
2017-08-05 15:53:01 +0700

    DB migration

--- README.md
+++ README.md
@@ -11,4 +11,7 @@ it provide endpoint for get/set post and alot of thing :D.
 ### installation
 1. clone this repo or load zip
 2. run `composer install` to install all dependency in `vendor`
-3. run npm install
+3. run `npm install`
+4. fill necessary data in .env file
+5. run `php artisan migrate` to migrate al db (make sure you provide DB auth in .env file)
+
--- database/migrations/2017_08_05_081151_post_table.php
+++ database/migrations/2017_08_05_081151_post_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class PostTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('title')->index();
+            $table->string('body');
+            $table->integer('user_id');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
             $table->timestamp('deleted_at')->nullable();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
